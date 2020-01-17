d20d42e14a8d63f1cc83d49abd104553d750cf1e
Manuel Goncalves <man281192@gmail.com>
2020-01-17 11:52:18 -0300

    Admin dashboard to users

--- users/models.py
+++ users/models.py
@@ -6,11 +6,11 @@ class Profile(models.Model):
     """
     Proxy model that extends the base data with other information.
     """
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    website = models.URLField(max_length=200,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200, blank=True)
     biography = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=20,blank=True)
-    picture = models.ImageField(upload_to='users/pictures',blank = True, null= True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    picture = models.ImageField(upload_to='users/pictures', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
